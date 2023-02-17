c253b021c8ea80e40b77a4600c8af2668bf86075
martinmmi <martin.mittrenga@t-online.de>
2023-02-17 09:14:13 +0100

    Init

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,17 @@
 #include <Arduino.h>
+#include <U8g2lib.h>
+#include <LoRa.h>
+#include <Pangodream_18650_CL.h>
+#include <bitmaps.h>
+
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
 
 void setup() {
   // put your setup code here, to run once:
